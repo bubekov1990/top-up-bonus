095b21dc7c3257df007906d9a226bbf4617958a4
bubekov1990 <bobbob1990@mail.ru>
2022-02-14 22:35:22 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,22 @@ public class Main {
         // Объявляете переменные для входных данных и
         // параметров программы: начального счёта,
         // суммы пополнения и тп
+        int check = 100;
+        int replenishment = 200;
+        int payment = check + replenishment / 100;
+        boolean a = check + replenishment > 1100;
 
         // Условным оператором проверяете превысила ли
         // сумма пополнения порог и для этих двух разных
         // сценариев рассчитываете сумму бонуса и выводите
         // на экран.
+        int replenished;
+        if (a) {
+            replenished = check + replenishment + payment;
+        } else {
+            replenished = check + replenishment;
+        }
+        System.out.println(a);
+
     }
 }
