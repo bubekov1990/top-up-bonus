6d7b21b2d0065463f20a0964b0ad829a9b837214
bubekov1990 <bobbob1990@mail.ru>
2022-04-19 00:33:25 +0300

    New commit

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
-        // Объявляете переменные для входных данных и
-        // параметров программы: начального счёта,
-        // суммы пополнения и тп
-
         int balance = 100;
-        int replenishment = 500;
-        int min = 1000;
-        boolean replenishes = true;
-
-        // Условным оператором проверяете превысила ли
-        // сумма пополнения порог и для этих двух разных
-        // сценариев рассчитываете сумму бонуса и выводите
-        // на экран.
-
-        int sum = balance + replenishment;
-        int bonus = replenishment / 100;
-        int percent = replenishes ? bonus : sum;
-        int check = percent + sum;
-        if (sum > min) {
-
+        int replenishment = 100;
+        int bonus;
+        if (replenishment > 1000) {
+            bonus = replenishment / 100;
         } else {
-            check = sum;
+            bonus = 0;
         }
-
-        System.out.println(check);
-
+        int finalBalance = balance + replenishment + bonus;
+        System.out.println(" Баланс " + finalBalance + " Бонус " + bonus);
     }
+
 }
